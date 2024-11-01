032cb0a248c91d477078d2628f3bb381654a3712
Ayush Khati <ayushiskhati@email.com>
2024-11-01 10:08:39 +0530

    Fix path to WrapAsync.js

--- routes/listing.js
+++ routes/listing.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const wrapAsync = require('../utils/WrapAsync.js'); 
+const WrapAsync = require('../utils/WrapAsync.js'); 
 const Listing = require('../models/listing.js');
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
 const listingController = require('../controllers/listings.js');
@@ -10,22 +10,22 @@ const upload = multer({storage});
 
 
 router.route("/")
-    .get(wrapAsync(listingController.index))
-    .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
+    .get(WrapAsync(listingController.index))
+    .post(isLoggedIn, upload.single("listing[image]"), validateListing, WrapAsync(listingController.createListing));
 
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
-    .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
-    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+    .get(WrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, WrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, WrapAsync(listingController.destroyListing));
 
 
 
 // Edit Route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+router.get("/:id/edit", isLoggedIn, isOwner, WrapAsync(listingController.renderEditForm));
 
 module.exports = router;
 
